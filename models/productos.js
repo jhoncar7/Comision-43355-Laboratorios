cc9c92d836262d38c8725f33b247560dbda73d15
Jhon Carreño <jhon7car@gmail.com>
2023-07-09 14:07:53 -0300

    fix

--- models/productos.js
+++ models/productos.js
@@ -5,7 +5,7 @@ const nameCollection = 'Producto'
 const ProductoShema = new Schema({
     title: { type: String, required: [true, 'El title del producto es obligatorio'] },
     description: { type: String, required: [true, 'La description del producto es obligatorio'] },
-    code: { type: String, required: [true, 'El code del producto es obligatorio'] },
+    code: { type: String, required: [true, 'El code del producto es obligatorio'],unique: true },
     price: { type: Number, required: [true, 'El price del producto es obligatorio'] },
     status: { type: Boolean, default: true },
     stock: { type: Number, required: [true, 'El stock del producto es obligatorio'] },
